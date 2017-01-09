99606def269428472da69b5e2eee77a184ca6697
Aviv Miron <avivmironinc@gmail.com>
2017-01-09 15:50:28 -0800

    Updated python code to use Flask

--- Main.py
+++ Main.py
@@ -1,2 +1,9 @@
-class Main:
-    print ("Hello World!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8000)
